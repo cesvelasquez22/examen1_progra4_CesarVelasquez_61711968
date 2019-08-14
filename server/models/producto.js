cdce13ea3f895f75e46120a678852a29f8def93a
cesvelasquez22 <ces.velasquez@hotmail.com>
2019-08-14 09:43:00 -0600

    My commit

--- server/models/producto.js
+++ server/models/producto.js
@@ -5,4 +5,19 @@ let Schema = mongoose.Schema;
 //Definir el esquema para el modelo Producto (Valor 5 puntos)
 
 
+let usuarioSchema = new Schema({
+    nombre: {
+        type: String,
+        required: true
+    },
+    precio: {
+        type: Number,
+        required: true
+    },
+    creadoEn:{
+        type: Date,
+        default: Date.now()
+    }
+})
+
 module.exports = mongoose.model('Producto', usuarioSchema);
--- server/routes/producto.js
+++ server/routes/producto.js
@@ -9,5 +9,55 @@ app.get('/', (req, res) => {
 });
 
 // Deberá hacer aquí el método get para producto (Valor 5 puntos)
+app.get('/producto', (req, res) =>{
+    res.json('GET producto',)
+});
 
 // Deberá hacer aquí el método post para producto (Valor 5 puntos)
+app.post('/producto', (req, res)=>{
+    let body = req.body;
+
+    let producto = new Producto({
+        nombre: body.nombre,
+        precio: body.precio,
+        creadoEn: body.creadoEn
+    });
+
+    //Grabando en la DB
+    producto.save((err, productoDB) =>{
+        //Validar si hubo error
+        if(err){
+            res.status(400).json({
+                ok: false,
+                err
+            })
+        }
+
+        res.json({
+            ok: true,
+            producto: productoDB
+        });
+    });
+});
+
+app.put('/producto/:id', (req, res)=>{
+    let id = req.params.id;
+    let body = req.body;
+    //Traer el usuario que solicitamos por su ID
+    Producto.findByIdAndUpdate(id, body, {new: true}, (err, productoDB)=>{
+        //Validar algun error en el llamado
+        if(err){
+            res.status(400).json({
+                ok: false,
+                err
+            })
+        }
+
+        res.json({
+            ok: true,
+            producto: productoDB
+        });
+    });
+});
+
+module.exports = app;
